61236a278d779653738975cfd5a0d78f660933fd
guayax <coceteljuanpablo@gmail.com>
2025-08-15 21:08:20 -0500

    creacion del esqueleto de pagina web con conversion de lista de python a json

---
+++ backend/backend.py
@@ -0,0 +1,21 @@
+#unholy python backend.
+import os
+import json
+dir_path = "media/galeria"
+
+#Obtine una lista de los nombres de archivos dentro de la carpeta galeria.
+def ContarArchivos():
+    imagenes =[]
+    for imagen in os.listdir(dir_path):
+        #busca si el elemento seleccionado es una imagen de formato png o jpg.
+        if os.path.isfile(os.path.join(dir_path,imagen)) and (imagen.endswith(".jpg") or imagen.endswith(".png")):
+            imagenes.append(imagen)
+    return imagenes
+#convierte la lista de nombres de las imagenes en un archivo json.
+def ConversionJSON():
+    lista = ContarArchivos()
+    
+    with open("backend\data\ListaImagenes.json", "w") as archivo:
+        json.dump(lista, archivo)
+
+ConversionJSON()
